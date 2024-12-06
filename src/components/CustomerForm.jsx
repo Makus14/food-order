936044fb816c8719361be46717d94afbc3b67971
Makus14 <mesha.maksim14@gmail.com>
2024-12-06 13:36:40 +0300

    styling customer component

--- src/components/CustomerForm.jsx
+++ src/components/CustomerForm.jsx
@@ -1,14 +1,54 @@
 export default function CustomerForm({ handleBackClick }) {
   return (
     <form>
-      <div>
-        <label htmlFor="name">Name:</label>
-        <input id="name" type="text" placeholder="Your Name" required />
+      <div style={{ marginBottom: "20px" }}>
+        <label>Total amount:</label>
       </div>
-      <div>
-        <label htmlFor="address">Address:</label>
-        <input id="address" type="text" placeholder="Your Address" required />
+      <div className="control">
+        <label htmlFor="name">Full Name</label>
+        <input
+          id="name"
+          type="text"
+          placeholder="Your Name"
+          required
+          style={{ marginBottom: "5px" }}
+        />
+
+        <label htmlFor="address">E-Mail Address</label>
+        <input
+          id="email"
+          type="text"
+          placeholder="Your Email"
+          required
+          style={{ marginBottom: "5px" }}
+        />
+
+        <label htmlFor="address">Street</label>
+        <input
+          id="street"
+          type="text"
+          placeholder="Your Street"
+          required
+          style={{ marginBottom: "5px" }}
+        />
+
+        <div className="control-row" style={{ marginBottom: "5px" }}>
+          <div>
+            <label htmlFor="address">Poste Code</label>
+            <input
+              id="code"
+              type="text"
+              placeholder="Your Poste Code"
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="address">City</label>
+            <input id="city" type="text" placeholder="Your City" required />
+          </div>
+        </div>
       </div>
+
       <div className="modal-actions">
         <button className="text-button" type="button" onClick={handleBackClick}>
           Back
